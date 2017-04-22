21b64baeccf853852b494111ad9c4779cb021075
tabchis <nqglxs@hi2.in>
2017-04-22 20:32:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ sudo apt-get dist-upgrade
 sudo ppa-purge
 sudo service redis-server restart
 sudo apt-get install -y tor
+sudo apt-get install -y openvpn
+sudo apt-get install network-manager-openvpn
 sudo service tor start
-sudo export http_proxy=socks5://127.0.0.1:9150 https_proxy=socks5://127.0.0.1:9150
+sudo service openvpn start
+export http_proxy="socks5://127.0.0.1:9150"
+export https_proxy="socks5://127.0.0.1:9150"
 chmod 777 telegram-cli-1222
-chmod 777 anticrash.sh
+chmod 777 telegamer.sh
 RED='\033[0;31m'
 NC='\033[0m'
 CYAN='\033[0;36m'
