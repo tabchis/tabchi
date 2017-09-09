6c90b404397f21d4d855a0415ea7d9c5cfb5fcbe
tabchis <nqglxs@hi2.in>
2017-09-09 23:04:20 +0430

    Update base.lua

--- base.lua
+++ base.lua
@@ -18,10 +18,10 @@ reload()
 
 function tdcli_update_callback(data)
   update(data, tabchi_id)
-  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
+  if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and (data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) or redis:sismember("tabchi:" .. tabchi_id .. ":sudoers", data.message_.sender_user_id_)) then
     reload()
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*ربات مجدداً راه اندازی شد               @TE1EGAMER*", 1, "md")
-  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
+  elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and (data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) or redis:sismember("tabchi:" .. tabchi_id .. ":sudoers", data.message_.sender_user_id_)) then
     io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
      io.popen("chmod 777 telegamer.sh"):read("*all")
     reload()
